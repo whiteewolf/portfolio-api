8c26e0aa13b08a580f8cfcc86c54a78f610e93a6
whiteewolf <arog0660@gmail.com>
2024-06-01 22:34:37 +0300

    g

--- api/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-require("dotenv").config();
-
-router.get('/api', (req, res) => {
-    res.sendFile('index.html', { root: path.join(__dirname, '../../public') });
-});
-
-module.exports = router;
