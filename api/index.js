bcff1c5c93fc86edba0196147d17c380f36c2a0e
whiteewolf <arog0660@gmail.com>
2024-06-01 22:16:51 +0300

    sss

--- api/index.js
+++ api/index.js
@@ -1,3 +1,38 @@
-const app = require('./app');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
+const messageRoutes = require('./routes/messages');
+const authRoutes = require('./routes/auth');
+// const admin = require('firebase-admin');
+require('dotenv').config();
 
-module.exports = app;
+const app = express();
+const PORT = 3000;
+
+// const serviceAccount = require('./config/firebaseServiceAccountKey.json');
+
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://<your-database-name>.firebaseio.com" // Replace <your-database-name>
+// });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'your_session_secret', // Change this to a secure secret
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/messages', messageRoutes);
+app.use('/auth', authRoutes);
+
+app.get('/', async (req, res) => {
+    res.redirect('/auth/login')
+})
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
